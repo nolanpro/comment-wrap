a34a0c4890705178035af5fc88430b7303b4ed70
Nolan Ehrstrom <github@nolanpro.com>
2017-03-30 22:26:10 -0700

    Making progress here I think

--- index.ts
+++ index.ts
@@ -1,34 +1,46 @@
 import { Wrapper } from './src/wrapper';
 
-let text = "  # abc abc abc abc abc abc asdf asdf asdf asdf asdf adsf asdf asd3 asd2 asd1 asd0aw" +
-    "\n" + "  # def def def";
-let limit = 80;
+let limit = 20;
 let cursor_x = 80
 let cursor_y = 0; // not necessary, will always be 0 because we don't neet stuff above current line
 
 let wrapper = new Wrapper(limit);
-let result = wrapper.wrap(text, cursor_x, cursor_y);
 
-console.log("OLD TEXT", text);
-console.log(`OLD CURSOR ${cursor_x}, ${cursor_y}`);
-console.log("NEW TEXT", result.text);
-console.log(`NEW CURSOR ${result.cx}, ${result.cy}`);
+$(document).ready(function() {
+  let textarea = $("#input");
+  let output = $("#output");
+  textarea.keyup(() => {
+    let text: string = textarea.val();
 
-export function wrapText() {
-  console.log("TRIGGERED WRAPPED TEST!!!!");
+    let pos = getCursorXYFromInput(textarea);
+
+    let result = wrapper.wrap(text, pos.cx, pos.cy);
+    output.html(result.text.replace(/\n/g, "<br />"));
+
+    let inputPos = getInputPositionFromCursorXY(result.cx, result.cy);
+    let element = <HTMLInputElement>textarea[0];
+    element.setSelectionRange(inputPos, inputPos);
+  });
+});
+
+function getInputPositionFromCursorXY(cx: number, cy: number): number {
+  // IDK
+  return 0;
 }
 
-
-// function wrapText() {
-//   console.log("WRAPPING NOW");
-// }
-//
-// $(document).ready(function() {
-//     console.log( "ready!" );
-// });
-//
-
-// REPLACE atom.current_line with new_current_line
-// REPLACE atom.next_line with new_next_line
-// SET CURSOR
-// NOW RE-RUN THIS ON new_next_line (now current_line)
+function getCursorXYFromInput(textarea: JQuery): { cx: number, cy: number } {
+  let pos = textarea.prop("selectionStart");
+  let text: string = textarea.val();
+  let cx = pos;
+  let cy = 0;
+  let lines = text.split("\n");
+  lines.forEach((line: string, index: number) => {
+    if (cx > line.length) {
+      if (index+1 != lines.length) {
+        cx = cx - line.length - 1;
+        cy++;
+      }
+    }
+  });
+  return { cx: cx, cy: cy }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 // https://github.com/krausest/ts-webpack-hmr
+var webpack = require('webpack');
 module.exports = {
   entry: "./index.ts",
   context: __dirname,
@@ -10,17 +11,17 @@ module.exports = {
   },
   devtool: 'source-map',
   resolve: {
-    // Add '.ts' and '.tsx' as a resolvable extension.
     extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"],
-    alias: {
-      jquery: "jquery/src/jquery"
-    }
   },
   module: {
     loaders: [
-      // all files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'
       { test: /\.ts$/, loader: "ts-loader" },
     ]
   },
-  plugins: [],
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery'
+    })
+  ],
 }
